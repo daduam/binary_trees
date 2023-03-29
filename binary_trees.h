9d3db622ee89711b7a1fef5107cd2a131561333e
Joseph Ampadu <jkampadujnr@gmail.com>
2023-03-29 21:40:36 +0000

    Search for value in BST

--- binary_trees.h
+++ binary_trees.h
@@ -53,6 +53,7 @@ typedef struct binary_tree_s bst_t;
 
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 
 typedef struct binary_tree_s avl_t;
 
