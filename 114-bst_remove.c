9065ce6cea4f09fa6d0882ef7b29a497c74ce16b
Joseph Ampadu <jkampadujnr@gmail.com>
2023-03-30 01:04:06 +0000

    Remove node from BST

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,79 @@
 #include "binary_trees.h"
 
 /**
+ * find_minimum - Find node with smallest value in subtree
+ *
+ * @root: Pointer to the root of the subtree
+ * Return: Node with smallest value in subtree
+ */
+bst_t *find_minimum(bst_t *root)
+{
+	if (!root->left)
+		return (root);
+	return (find_minimum(root->left));
+}
+
+/**
+ * delete_node - Removes a node from the tree
+ *
+ * @root: Pointer to the root of the tree
+ * @node: Pointer to node to remove
+ * Return: Pointer to new root of tree after removing node
+ */
+bst_t *delete_node(bst_t *root, bst_t *node)
+{
+	bst_t *parent, *tmp;
+
+	parent = node->parent;
+	if (!node->left)
+	{
+		if (parent && parent->left == node)
+			parent->left = node->right;
+		else if (parent)
+			parent->right = node->right;
+		if (node->right)
+			node->right->parent = parent;
+		tmp = node->right;
+		free(node);
+		return (parent ? root : tmp);
+	}
+	if (!node->right)
+	{
+		if (parent && parent->left == node)
+			parent->left = node->left;
+		else if (parent)
+			parent->right = node->left;
+		if (node->left)
+			node->left->parent = parent;
+		tmp = node->left;
+		free(node);
+		return (parent ? root : tmp);
+	}
+	tmp = find_minimum(node->right);
+	node->n = tmp->n;
+	return (delete_node(root, tmp));
+}
+
+/**
+ * _bst_remove - bst_remove recursive helper
+ *
+ * @root: Pointer to the root node of the tree where you will remove a node
+ * @node: Pointer to current node of the tree
+ * @value: Value to remove in the tree
+ * Return: Value removed from the tree, or NULL
+ */
+bst_t *_bst_remove(bst_t *root, bst_t *node, int value)
+{
+	if (!node)
+		return (NULL);
+	if (value == node->n)
+		return (delete_node(root, node));
+	if (value < node->n)
+		return (_bst_remove(root, node->left, value));
+	return (_bst_remove(root, node->right, value));
+}
+
+/**
  * bst_remove - Removes a node from a Binary Search Tree
  *
  * @root: Pointer to the root node of the tree where you will remove a node
@@ -10,44 +83,7 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *tmp, *succ;
-
 	if (!root)
 		return (NULL);
-	if (value > root->n)
-	{
-		root->right = bst_remove(root->right, value);
-		return (root);
-	}
-	if (value < root->n)
-	{
-		root->left = bst_remove(root->left, value);
-		return (root);
-	}
-	if (!root->left)
-	{
-		tmp = root->right;
-		free(root);
-		return (tmp);
-	}
-	if (!root->right)
-	{
-		tmp = root->left;
-		free(root);
-		return (tmp);
-	}
-	tmp = root;
-	succ = root->right;
-	while (succ->left)
-	{
-		tmp = succ;
-		succ = succ->left;
-	}
-	if (tmp != root)
-		tmp->left = succ->right;
-	else
-		tmp->right = succ->right;
-	root->n = succ->n;
-	free(succ);
-	return (root);
+	return (_bst_remove(root, root, value));
 }
